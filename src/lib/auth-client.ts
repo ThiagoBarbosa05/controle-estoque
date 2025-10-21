0b71ad490e93f363f49ccc1ebc0a797f92081d7e
ThiagoBarbosa05 <thiagon16@hotmail.com>
2025-10-20 22:58:44 -0300

    cache

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  // baseURL: "https://controle-estoque-eight-eta.vercel.app",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://controle-estoque-eight-eta.vercel.app",
+  // baseURL: "http://localhost:3000",
 });
 
 export const { signIn, signUp, useSession, signOut } = authClient;
